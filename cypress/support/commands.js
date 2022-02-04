3a4968ecc3a2151e0c997c39b23ddc6e3fcf0bc5
borisfrenkel <bfrenkel@gmail.com>
2022-02-04 11:33:24 +0100

    ading custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,125 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+var authToken = null;
+var lastTokenTime = null;
+
+Cypress.Commands.add("GetAuthToken", () => {
+  const now = new Date().getTime();
+
+  if (lastTokenTime == null) {
+    lastTokenTime = now;
+  }
+
+  if (now - lastTokenTime >= Cypress.config().authTokenTimeOut) {
+    var formBody = [];
+    formBody.push("grant_type=client_credentials");
+    formBody = formBody.join("&");
+    cy.request({
+      method: "POST",
+      url:
+        "https://" +
+        Cypress.config().projectID +
+        ":" +
+        Cypress.config().projectAPIKey +
+        "@api.up42.com/oauth/token",
+      form: true,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: formBody,
+      redirect: "follow",
+    }).then((result) => {
+      authToken = JSON.stringify(result.body["data"]["accessToken"]);
+    });
+  }
+});
+
+Cypress.Commands.add("AddFlow", () => {
+  cy.GetAuthToken().then(() => {
+    var raw = JSON.stringify({
+      name: "QA coding challenge workflow123",
+      description: "Workflow description",
+    });
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + authToken);
+    myHeaders.append("Content-Type", "application/json");
+
+    cy.request({
+      method: "POST",
+      url:
+        "https://api.up42.com/projects/" +
+        Cypress.config().projectID +
+        "/workflows/",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    });
+  });
+});
+
+Cypress.Commands.add("CreateJobAndRunJob", (workFlowId) => {
+  cy.GetAuthToken().then(() => {
+    var raw = JSON.stringify({
+      "nasa-modis:1": {
+        time: "2018-12-01T00:00:00+00:00/2020-12-31T23:59:59+00:00",
+        limit: 1,
+        zoom_level: 9,
+        imagery_layers: ["MODIS_Terra_CorrectedReflectance_TrueColor"],
+        bbox: [13.365373, 52.49582, 13.385796, 52.510455],
+      },
+      "sharpening:1": {
+        strength: "medium",
+      },
+    });
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + authToken);
+    myHeaders.append("Content-Type", "application/json");
+
+    cy.request({
+      method: "POST",
+      url:
+        "https://api.up42.com/projects/" +
+        Cypress.config().projectID +
+        "/workflows/" +
+        workFlowId +
+        "/jobs",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    });
+  });
+});
+
+Cypress.Commands.add("GetJobDetails", (jobId) => {
+  cy.GetAuthToken().then(() => {
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + authToken);
+
+    cy.request({
+      method: "GET",
+      url:
+        "https://api.up42.com/projects/" +
+        Cypress.config().projectID +
+        "/jobs/" +
+        jobId,
+      headers: myHeaders,
+      redirect: "follow",
+    });
+  });
+});
+
+Cypress.Commands.add("DeleteWorkflow", (workFlowId) => {
+  cy.GetAuthToken().then(() => {
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + authToken);
+
+    cy.request({
+      method: "DELETE",
+      url:
+        "https://api.up42.com/projects/" +
+        Cypress.config().projectID +
+        "/workflows/" +
+        workFlowId,
+      headers: myHeaders,
+      redirect: "follow",
+    });
+  });
+});
